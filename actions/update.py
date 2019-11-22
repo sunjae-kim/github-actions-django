6ca817df5c5a55c64e86143510f52242272dcaea
sunjae-kim <kimsj9484@gmail.com>
2019-11-22 22:13:48 +0900

    action test

--- actions/update.py
+++ actions/update.py
@@ -1,3 +1,8 @@
+from movies.apis import fetch_movies
 from datetime import datetime
+from pprint import pprint
 
+movies = fetch_movies()
 print('Hello World at', datetime.now())
+print()
+pprint(movies)
