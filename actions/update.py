0eaf0c751b91e10d1747175114e3f1e8e07d90c1
sunjae-kim <kimsj9484@gmail.com>
2019-11-23 11:22:27 +0900

    Fix issue on update

--- actions/update.py
+++ actions/update.py
@@ -1,6 +1,15 @@
-from movies.apis import fetch_movies
-from datetime import datetime
+import requests
+from decouple import config
+from datetime import datetime, timedelta
 from pprint import pprint
+
+def fetch_movies():
+    API_KEY = config('MOVIE_API_KEY')
+    API_URL = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json'
+    REQUEST_URL = f'{API_URL}?key={API_KEY}&targetDt={datetime.strftime(datetime.now() - timedelta(1), "%Y%m%d")}'
+    response = requests.get(REQUEST_URL)
+    movies = response.json().get('boxOfficeResult').get('dailyBoxOfficeList')
+    return movies
 
 movies = fetch_movies()
 print('Hello World at', datetime.now())
