a0623507e13eb99ebf7f653ed06eeb3431498ec2
sunjae-kim <kimsj9484@gmail.com>
2019-11-23 15:56:06 +0900

    deploy test

--- actions/update.py
+++
@@ -1,7 +0,0 @@
-import requests
-from datetime import datetime
-
-response = requests.get('https://dog.ceo/api/breeds/image/random')
-
-print('Hello World at', datetime.now())
-print(response.json())
