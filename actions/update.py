5bcf3d54374dedee1e40420b93faebdc0aa7649e
sunjae-kim <kimsj9484@gmail.com>
2019-11-23 11:31:40 +0900

    Install requests module when action is triggered

--- actions/update.py
+++ actions/update.py
@@ -1,17 +1,7 @@
 import requests
-from decouple import config
-from datetime import datetime, timedelta
-from pprint import pprint
+from datetime import datetime
 
-def fetch_movies():
-    API_KEY = config('MOVIE_API_KEY')
-    API_URL = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json'
-    REQUEST_URL = f'{API_URL}?key={API_KEY}&targetDt={datetime.strftime(datetime.now() - timedelta(1), "%Y%m%d")}'
-    response = requests.get(REQUEST_URL)
-    movies = response.json().get('boxOfficeResult').get('dailyBoxOfficeList')
-    return movies
+response = requests.get('https://dog.ceo/api/breeds/image/random')
 
-movies = fetch_movies()
 print('Hello World at', datetime.now())
-print()
-pprint(movies)
+print(response.json())
