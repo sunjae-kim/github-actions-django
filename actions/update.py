7353d5f7360f966f998bfab728df5085b5377d2e
sunjae-kim <kimsj9484@gmail.com>
2019-11-25 15:31:57 +0900

    update db weekly

--- actions/update.py
+++ actions/update.py
@@ -2,3 +2,4 @@
 import requests
 
 response = requests.get('http://github-actions-test.jvnsgh2u35.ap-northeast-2.elasticbeanstalk.com/movies/update-db/')
+print(response.json())
--- movies/apis.py
+++ movies/apis.py
@@ -5,8 +5,12 @@ from datetime import datetime, timedelta
 
 def fetch_movies():
     API_KEY = config('MOVIE_API_KEY')
-    API_URL = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json'
-    REQUEST_URL = f'{API_URL}?key={API_KEY}&targetDt={datetime.strftime(datetime.now() - timedelta(1), "%Y%m%d")}'
+    API_URL = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json'
+    REQUEST_URL = f'{API_URL}?key={API_KEY}&targetDt={datetime.strftime(datetime.now() - timedelta(10), "%Y%m%d")}'
     response = requests.get(REQUEST_URL)
-    movies = response.json().get('boxOfficeResult').get('dailyBoxOfficeList')
+    movies = response.json().get('boxOfficeResult').get('weeklyBoxOfficeList')
     return movies
+
+if __name__ == "__main__":
+    movies = fetch_movies()
+    print(movies)
